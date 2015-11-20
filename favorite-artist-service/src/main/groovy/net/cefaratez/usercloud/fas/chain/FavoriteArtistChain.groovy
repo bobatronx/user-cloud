2adfc52893ddbe538e4c8a896503b3d946970294
James Cefaratti <jcefaratti@gmail.com>
2015-11-20 12:05:35 -0500

    Remove unused import

--- favorite-artist-service/src/main/groovy/net/cefaratez/usercloud/fas/chain/FavoriteArtistChain.groovy
+++ favorite-artist-service/src/main/groovy/net/cefaratez/usercloud/fas/chain/FavoriteArtistChain.groovy
@@ -3,14 +3,13 @@ package net.cefaratez.usercloud.fas.chain
 import groovy.json.JsonSlurper
 import net.cefaratez.usercloud.fas.service.MapProfileUtil
 import org.springframework.stereotype.Component
-import org.springframework.web.util.UriComponentsBuilder
 import ratpack.groovy.handling.GroovyChainAction
 import ratpack.http.HttpUrlBuilder
 import ratpack.http.client.HttpClient
 import ratpack.http.client.ReceivedResponse
 import rx.Observable
-import static ratpack.jackson.Jackson.json
 
+import static ratpack.jackson.Jackson.json
 
 @Component
 class FavoriteArtistChain extends GroovyChainAction {
